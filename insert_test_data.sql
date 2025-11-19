769d00f1c3a23cbd6c8f637cb7b16ab242531d98
Yameen <myame001@gold.ac.uk>
2025-11-19 14:15:35 +0000

    updated insert test data

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('The Lean Start Up', 27.99),('Romeo and Julliet', 12.99) ;
+
+INSERT INTO users (username, first_name, last_name, email, hashedPassword)
+SELECT 'gold', 'Bertie', 'Gold', 'gold@berties.com', '$2b$10$DjgNZ/V9gJjUXGIOahU3weSqDP.Wj/m8oOenTFSOdKqUixGUXDV1m'
